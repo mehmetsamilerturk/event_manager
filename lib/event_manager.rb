d6327b666221d757604f0b3fe7ee7f9818df76c8
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2021-12-24 23:46:56 +0300

    gives hours most people registered

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,17 +2,30 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
+def parse_date(date)
+  date_split = date.split(' ')[0].split('/')
+  date_formatted = date_split.map do |value|
+    value.rjust(2, '0')
+  end
+
+  date_formatted = date_formatted.reverse.insert(1, date_formatted.reverse.delete_at(2))
+  date_formatted.delete_at(-1)
+  date_formatted = date_formatted.join('/').concat(" #{date.split(' ')[1]}")
+
+  Time.parse(date_formatted)
+end
+
 def clean_homephone(homephone)
   #   If the phone number is less than 10 digits, assume that it is a bad number(done)
   #   If the phone number is 10 digits, assume that it is good(done)
   #   If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits(done)
   #   If the phone number is 11 digits and the first number is not 1, then it is a bad number(done)
   #   If the phone number is more than 11 digits, assume that it is a bad number(done)
-  homephone_digits = homephone.gsub(/[^\d]/, "")
+  homephone_digits = homephone.gsub(/[^\d]/, '')
   if homephone_digits.size < 10
     homephone_digits.ljust(10, '0')
   elsif homephone_digits.size > 10
-    if homephone_digits.size == 11 
+    if homephone_digits.size == 11
       if homephone_digits[0] == '1'
         homephone_digits[1..10]
       else
@@ -63,16 +76,25 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+hours = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   homephone = clean_homephone(row[:homephone])
+  date = parse_date(row[:regdate])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
-  puts homephone
+  #puts homephone
+
+  hours.push(date.hour)
 end
+peak_hours = hours.each_with_object(Hash.new(0)) do |hour, result|
+  result[hour] += 1
+end
+peak_hours_sorted = peak_hours.sort_by { |_key, value| value }.reverse.to_h
+puts "Hours most people registered: #{peak_hours_sorted.keys[0, 2].join(' and ')}"
